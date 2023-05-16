6c8fbfbacbe33503c87311fddc6acb0cc4514f33
hamid reza <reza.attar1375@gmail.com>
2023-05-16 23:34:39 +0430

    endpwa

--- configPrj/public/sw.js
+++ configPrj/public/sw.js
@@ -4,7 +4,7 @@ const preCache = [
   '/index.html',
   '/',
   '/css/font.css',
-  
+
   '/fonts/AntDesign.ttf',
   '/fonts/B Baran Regular.ttf',
   '/fonts/FontAwesome5_Brands.ttf',
@@ -15,12 +15,63 @@ const preCache = [
   '/fonts/IRANSansWeb.ttf',
   '/fonts/MaterialIcons.ttf',
   '/fonts/Yekan Bakh Regular.ttf',
-  
-  "/static/css/main.b3565330.css",
-  "/static/js/main.397ce9ac.js",
+
+  "/manifest.json",
+  "/app.js",
+
+  // "/src/index.js",
+  // "/src/App.js",
 
   '/logo.png',
+  "/static/media/logo.18974f15ded44566f065.png",
+  '/64x64.png',
+  '/192x192.png',
+  '/512x512.png',
+
+  "/static/css/main.fdaee16a.css",
+  "/static/js/main.fe66d8ce.js",
+
+  "/static/css/269.92b20761.chunk.css",
+  '/static/css/529.92b20761.chunk.css',
+  '/static/js/12.801d5769.chunk.js',
+  '/static/js/529.18874ba9.chunk.js',
+  '/static/js/935.e8976ac1.chunk.js',
+  '/static/js/269.f2c537b8.chunk.js',
+  "/static/js/735.1c39d47e.chunk.js",
+  "/static/js/517.260cf234.chunk.js",
+  "/static/js/979.f244358d.chunk.js",
+  "/static/css/232.92b20761.chunk.css",
+  "/static/js/232.87b10e59.chunk.js",
+  "/static/js/818.4b661237.chunk.js",
+  "/static/js/707.cebb4045.chunk.js",
+  "/static/js/881.7001b440.chunk.js",
+  "/static/js/365.e297f1a3.chunk.js",
+  "/static/js/102.e8c46dc3.chunk.js",
+  "/static/js/510.42c97390.chunk.js",
+  "/static/js/496.1a783828.chunk.js",
+  "/static/css/main.fdaee16a.css.map",
+  "/static/js/main.fe66d8ce.js.map",
+  "/static/css/269.92b20761.chunk.css.map",
+  "/static/js/269.f2c537b8.chunk.js.map",
+  "/static/css/529.92b20761.chunk.css.map",
+  "/static/js/529.18874ba9.chunk.js.map",
+  "/static/js/935.e8976ac1.chunk.js.map",
+  "/static/js/12.801d5769.chunk.js.map",
+  "/static/js/735.1c39d47e.chunk.js.map",
+  "/static/js/517.260cf234.chunk.js.map",
+  "/static/js/979.f244358d.chunk.js.map",
+  "/static/css/232.92b20761.chunk.css.map",
+  "/static/js/232.87b10e59.chunk.js.map",
+  "/static/js/818.4b661237.chunk.js.map",
+  "/static/js/707.cebb4045.chunk.js.map",
+  "/static/js/881.7001b440.chunk.js.map",
+  "/static/js/365.e297f1a3.chunk.js.map",
+  "/static/js/102.e8c46dc3.chunk.js.map",
+  "/static/js/510.42c97390.chunk.js.map",
+  "/static/js/496.1a783828.chunk.js.map",
+  
 ];
+
 
 self.addEventListener('install', (ev) => {
   //installed
