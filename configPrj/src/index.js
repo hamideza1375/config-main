5b180084c1ccd658badb9cc351a33a7b8a49fd48
hamid reza <reza.attar1375@gmail.com>
2023-07-18 20:34:13 +0330

    frm

--- configPrj/src/index.js
+++ configPrj/src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from "react-dom/client";
 // import ReactDOMServer from "react-dom/server";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import { NavigationContainer } from '@react-navigation/native';
 // import SingleProduct from './views/client/Home';
 
 // console.log(ReactDOMServer.renderToString(
+//   <NavigationContainer>
 //     <SingleProduct />
+//   </NavigationContainer>
 // ));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
