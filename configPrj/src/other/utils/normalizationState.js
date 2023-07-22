0bd30a847c26c3c7a7392faf1970adcb8b3586f0
hamid reza <reza.attar1375@gmail.com>
2023-07-22 19:35:50 +0330

    normalize

--- configPrj/src/other/utils/normalizationState.js
+++ configPrj/src/other/utils/normalizationState.js
@@ -1,9 +1,23 @@
-export default function normalizationState(array) {
+export default function normalizationState(array, setarray) {
   let obj = {}
   array.forEach(a => (obj[a._id] = a))
   this.value = () => { return Object.values(obj) }
-  this.remove = (id) => { delete obj[id] }
+  this.remove = (id) => { delete obj[id]; setarray({...this}) }
   this.find = (id) => { return obj[id] ? obj[id] : {} }
   this.findIndex = (id) => { return Object.keys(obj).findIndex((_id) => (_id === id)) }
-  this.changeIndex = (id) => { return obj[id] ? Object.values(obj)[id] :{} }
+  this.changeIndex = (id) => { return (array[id] ? Object.values(obj)[id] :{}) }
+  this.save = () => { setarray({...this}) }
+  // this.save = () => { setarray({value:this.value, remove:this.remove, find:this.find, findIndex:this.findIndex, changeIndex:this.changeIndex, save:this.save}) }
 }
+
+// export default function normalizationState(array, setarray) {
+//   let obj = {}
+//   array.forEach(a => (obj[a._id] = a))
+//   this.value = () => { return Object.values(obj) }
+//   this.remove = (id) => { delete obj[id] }
+//   this.find = (id) => { return obj[id] ? obj[id] : {} }
+//   this.findIndex = (id) => { return Object.keys(obj).findIndex((_id) => (_id === id)) }
+//   this.changeIndex = (id) => { return array[id] ? Object.values(obj)[id] :{} }
+//   this.save = () => { setarray({...this}) }
+//   // this.save = () => { setarray({value:this.value, remove:this.remove, find:this.find, findIndex:this.findIndex, changeIndex:this.changeIndex, save:this.save}) }
+// }
