52312901cb4e8f631fd69b8c7eee545857686d44
hamid reza <reza.attar1375@gmail.com>
2023-07-22 16:36:26 +0330

    category

--- configPrj/src/views/Components/Category.js
+++ configPrj/src/views/Components/Category.js
@@ -1,3 +1,48 @@
+// import { Column, Img, P, Press, ScrollSlider } from '../../other/Components/Html'
+// import LinearGradient from '../../other/Components/other/LinearGradient'
+// import { localhost } from '../../other/utils/axios/axios'
+
+// const Category = (p) => {
+//   p._client.getCategory()
+
+//   const del = () => {
+//     let category = { ...p.category }
+//     category.remove('64b00db5301acd6480754274')
+//     category.find("64b0530c57a2eb21a26483af").title = 3333333444
+//     category.changeIndex(10).title = 'newTitle'
+//     p.setcategory(category)
+//   }
+
+
+//   return (
+//     <Column onClick={del} minh={150} >
+//       <Column fd='row' ai='center' >
+//         <P mr={15} mt={12} fs={15} mb={-15} color='#444' >دسته ها</P>
+//         <LinearGradient nativeStart={{ x: 1.5, y: 1.5 }} webStart={{ x: 7 }} colors={['#f5f', '#505']} style={{ width: '50%', height: 1, flexGrow: 1, marginTop: 28, marginHorizontal: 25 }} ></LinearGradient>
+//       </Column>
+//       <ScrollSlider
+//         h={180}
+//         ccStyle={{ height: 180, justifyContent: 'center', alignSelf: 'center' }}
+//         data={p.category?.value() ? p.category.value() : []}
+//         renderItem={({ item, index }) => (
+//           <Column w={105} h={115} as='center' fg={1} >
+//             <Column w={90} h={95} as='center'>
+//               <LinearGradient nativeStart={{ x: 1.5, y: 1.5 }} webStart={{ x: 7 }} colors={['#f5f', '#505', '#f5f']} style={{ borderRadius: 100, width: 90, height: 90, maxHeight: 90, justifyContent: 'center', alignItems: 'center' }} >
+//                 <Column w={84} h={84} style={{ borderRadius: 100, backgroundColor: 'white', position: 'absolute' }} >
+//                   {item.imageUrl && <Press f={1} onClick={() => { p.navigation.navigate('Products', { id: item._id }) }}><Img f={1} br={100} src={{ uri: `${localhost}/upload/category/${item.imageUrl}` }} /></Press>}
+//                 </Column>
+//               </LinearGradient>
+//             </Column>
+//             <P as='center' mt='auto' fs={14} fw='600' color='#229b' >{item.title}</P>
+//           </Column>
+//         )}
+//       />
+
+//     </Column>)
+// }
+
+// export default Category
+
 import { useEffect } from 'react';
 import { Column, Img, P, Press, ScrollSlider } from '../../other/Components/Html'
 import LinearGradient from '../../other/Components/other/LinearGradient'
